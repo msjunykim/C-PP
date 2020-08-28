c199d787f59e2914302d7520db934e2357e4a150
Mithona Kim <mithonakim@gmail.com>
2020-08-28 22:36:02 +0700

    Add files via upload

---
+++ C-PP.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//   cout << "Hello world!\n\n";
+//   cout << "I am Learning C++";
+
+//   cout << "Hello World!" << endl;
+//   cout << "I am Learning C++";
+  
+
+  // C++ Comments
+  // This is a comment
+//   cout << "Hello single comment!";
+
+  /* The code below will print the words Hello World!
+	to the screen, and it is amazing */
+//   cout << "Hello multiple comment!";
+
+
+	// Declaring (Creating) Variables
+	// int myNum = 15;
+	// cout << myNum;
+
+	// declare a variable without assigning the value,
+	// int myNum;
+	// myNum = 20;
+	// cout << myNum;
+
+	// Note that if you assign a new value to an existing variable, it will overwrite the previous value:
+
+	// int myNum = 15;  // myNum is 15˜˜˜˜˜˜˜
+	// myNum = 10;  // Now myNum is 10
+	// cout << myNum;  // Outputs 10
+
+	
+	// Other Type 
+	// int myNum = 5;               // Integer (whole number without decimals)
+	// double myFloatNum = 5.99;    // Floating point number (with decimals)
+	// char myLetter = 'D';         // Character
+	// string myText = "Hello";     // String (text)
+	// bool myBoolean = true;       // Boolean (true or false)
+
+	// cout << myNum;
+	// cout << myFloatNum;
+	// cout << myLetter;
+	// cout << myText;
+	// cout << myBoolean;
+
+	// Display Variables
+
+	// int myAge = 35;
+	// cout << "I am " << myAge << " years old.";
+
+	// Add Variables Together
+	int x = 5;
+	int y = 6;
+	int sum = x + y;
+	cout << sum;
+
+	// Declare Many Variables
+	// int x = 5, y = 6, z = 50;
+	// cout << x + y + z;
+
+
+	// C++ Identifiers
+	// Good
+	// int minutesPerHour = 60;
+
+	// OK, but not so easy to understand what m actually is
+	// int m = 60;
+
+	// C++ Constants
+	// const int myNum = 15;  // myNum will always be 15
+	// myNum = 10;  // error: assignment of read-only variable 'myNum'
+
+	// const int minutesPerHour = 60;
+	// const float PI = 3.14;
+
+	// C++ User Input
+	// int x; 
+	// cout << "Type a number: "; // Type a number and press enter
+	// cin >> x; // Get user input from the keyboard
+	// cout << "Your number is: " << x; // Display the input value
+
+  return 0;
+	
+}
